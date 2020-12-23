6d199c04c2f2925920f12816d4dd473a30ae3643
adjlyadv <994845300@qq.com>
2020-12-23 16:21:51 +0800

    add scss loader

--- src/containers/BasciLayout.tsx
+++ src/containers/BasciLayout.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
-import EditorContainer from './EditorContainer'
-import '../style/index.less';
+import EditorContainer from './EditorContainer';
+import '../style/index.scss';
 
 export default class BasicLayout extends React.Component {
 
--- src/containers/EditorContainer.tsx
+++ src/containers/EditorContainer.tsx
@@ -4,9 +4,14 @@ import Path from '../elements/path';
 
 export default class EditorContainer extends React.Component {
   
+
+  handleEditorClick = (event: any) => {
+    console.log(event)
+  }
+
   render() {
     return(
-      <svg width="500" height="500"> 
+      <svg onClick={this.handleEditorClick} width="500" height="500"> 
         <Path />
       </svg>
     )
