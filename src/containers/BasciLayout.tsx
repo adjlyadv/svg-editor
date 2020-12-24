a1dbab8e808f645c48507f672fdd6b3d505b3c23
Scentist25 <54299113+Scentist25@users.noreply.github.com>
2020-12-24 16:02:36 +0800

    工具栏布局修改 (#6)

--- src/containers/BasciLayout.tsx
+++ src/containers/BasciLayout.tsx
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react';
 import EditorContainer from './EditorContainer';
 import StatusContainer from './StatusContainer';
 import '../style/index.scss';
+import ToolbarContainer from "./ToolbarContainer";
 
 export default class BasicLayout extends React.Component {
 
   render() {
     return(
       <Fragment>
+        <ToolbarContainer />
         <EditorContainer />
         <StatusContainer />
       </Fragment>
