e8db71ee336936fac72160e09ceaaf6ccc98511b
Scentist25 <544708961@qq.com>
2020-12-24 15:57:58 +0800

    工具栏布局修改

--- src/containers/BasciLayout.tsx
+++ src/containers/BasciLayout.tsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import EditorContainer from './EditorContainer';
 import '../style/index.scss';
+import ToolbarContainer from "./ToolbarContainer";
 
 export default class BasicLayout extends React.Component {
 
   render() {
     return(
       <Fragment>
+        <ToolbarContainer />
         <EditorContainer />
         
       </Fragment>
--- src/containers/ToolbarContainer.tsx
+++ src/containers/ToolbarContainer.tsx
@@ -1,13 +1,32 @@
-import React, { Fragment } from 'react';
-
+import React from 'react';
+import '../style/ToolbarContainer.scss'
+import circle from '../asset/circle.svg'
+import pen from '../asset/pen.svg'
+import mouse from '../asset/mouse.svg'
+import line from '../asset/line.svg'
+import rectangle from '../asset/rectangle.svg'
 export default class ToolbarContainer extends React.Component {
 
-  render() {
-    return(
-      <Fragment>
-        
-      </Fragment>
-    )
-  }
+    render() {
+        return (
+                <div className="Toolbar">
+                    <div className="Toolitem" >
+                        <img className="itmeicon" src={mouse}/>
+                    </div>
+                    <div className="Toolitem">
+                        <img className="itmeicon" src={circle}/>
+                    </div>
+                    <div className="Toolitem">
+                        <img className="itmeicon" src={rectangle}/>
+                    </div>
+                    <div className="Toolitem">
+                        <img className="itmeicon" src={line}/>
+                    </div>
+                    <div className="Toolitem">
+                        <img className="itmeicon" src={pen}/>
+                    </div>
 
+                </div>
+        )
+    }
 }
