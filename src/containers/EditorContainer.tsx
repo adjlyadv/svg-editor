c9524ea7871142b17573da5548cdbfc7c0ab4b98
Shirai Kuroko <adjlyadv@gmail.com>
2021-01-03 17:13:40 +0800

    add uistore (#7)

--- src/containers/EditorContainer.tsx
+++ src/containers/EditorContainer.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { observer } from 'mobx-react';
+import { UIStore } from '../stores/UIStore';
 import Path from '../elements/path';
 
-
+@observer
 export default class EditorContainer extends React.Component {
-  
 
   handleEditorClick = (event: any) => {
     console.log(event)
   }
 
   render() {
+
+    const editorInfo = UIStore.editorInfo;
+
     return(
-      <svg onClick={this.handleEditorClick} width="500" height="500"> 
+      <svg onClick={this.handleEditorClick} width={editorInfo.width} height={editorInfo.height}> 
         <Path />
       </svg>
     )
