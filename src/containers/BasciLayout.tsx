3a7749fdc951ecc845ce4dd20371ea54ecf41c61
Scentist25 <54299113+Scentist25@users.noreply.github.com>
2021-01-07 18:26:43 +0800

    调整工具栏样式，增加点击后的选中效果 (#9)
    * 调整工具栏样式，增加点击后的选中效果
    
    * 选中样式优化
    
    * 加入interface，修改样式定义
    
    Co-authored-by: Shirai Kuroko <adjlyadv@gmail.com>

--- src/containers/BasciLayout.tsx
+++ src/containers/BasciLayout.tsx
@@ -4,12 +4,25 @@ import StatusContainer from './StatusContainer';
 import '../style/index.scss';
 import ToolbarContainer from "./ToolbarContainer";
 
-export default class BasicLayout extends React.Component {
+export default class BasicLayout extends React.Component<any,any> {
+
+    constructor(props:any){
+        super(props)
+        this.state={
+            currentTool:"mouse",
+        }
+    }
+
+    setCurrenttool(msg:any){
+        this.setState({
+            currentTool:msg,
+        })
+    }
 
   render() {
     return(
       <Fragment>
-        <ToolbarContainer />
+        <ToolbarContainer currentTool={this.state.currentTool} set={this.setCurrenttool.bind(this)}/>
         <EditorContainer />
         <StatusContainer />
       </Fragment>
--- src/containers/ToolbarContainer.tsx
+++ src/containers/ToolbarContainer.tsx
@@ -5,27 +5,40 @@ import pen from '../asset/pen.svg'
 import mouse from '../asset/mouse.svg'
 import line from '../asset/line.svg'
 import rectangle from '../asset/rectangle.svg'
-export default class ToolbarContainer extends React.Component {
+
+interface State{
+
+}
+interface Prpos{
+    currentTool:string;
+    set:(arg0: string)=>void;
+}
+
+export default class ToolbarContainer extends React.Component<Prpos,State> {
+
+    handleClick(e:any){
+        this.props.set(e.target.id)
+    }
 
     render() {
-        return (
-                <div className="Toolbar">
-                    <div className="Toolitem" >
-                        <img className="itmeicon" src={mouse} alt="mouse"/>
-                    </div>
-                    <div className="Toolitem">
-                        <img className="itmeicon" src={circle} alt="circle"/>
-                    </div>
-                    <div className="Toolitem">
-                        <img className="itmeicon" src={rectangle} alt="rectangle"/>
-                    </div>
-                    <div className="Toolitem">
-                        <img className="itmeicon" src={line} alt="line"/>
-                    </div>
-                    <div className="Toolitem">
-                        <img className="itmeicon" src={pen} alt="pen"/>
-                    </div>
 
+        return (
+                <div className="toolbar">
+                    <div className="toolitem" onClick={this.handleClick.bind(this)} >
+                        <img className={this.props.currentTool==="mouse"?"itmeIconselect":"itmeIcon"} id="mouse"  alt="mouse" src={mouse}/>
+                    </div>
+                    <div className="toolitem" onClick={this.handleClick.bind(this)}>
+                        <img className={this.props.currentTool==="circle"?"itmeIconselect":"itmeIcon"} id="circle" alt="circle" src={circle}/>
+                    </div>
+                    <div className="toolitem" onClick={this.handleClick.bind(this)}>
+                        <img className={this.props.currentTool==="rectangle"?"itmeIconselect":"itmeIcon"} id="rectangle" alt="rectangle" src={rectangle}/>
+                    </div>
+                    <div className="toolitem" onClick={this.handleClick.bind(this)}>
+                        <img className={this.props.currentTool==="line"?"itmeIconselect":"itmeIcon"} id="line" alt="line" src={line}/>
+                    </div>
+                    <div className="toolitem" onClick={this.handleClick.bind(this)}>
+                        <img className={this.props.currentTool==="pen"?"itmeIconselect":"itmeIcon"} id="pen" alt="pen" src={pen}/>
+                    </div>
                 </div>
         )
     }
