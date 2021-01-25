f97e6e8484a53c36e80bd5e4ee49613690ef13d1
adjlyadv <994845300@qq.com>
2021-01-25 18:51:58 +0800

    change to function component

--- src/containers/ToolbarContainer.tsx
+++ src/containers/ToolbarContainer.tsx
@@ -6,40 +6,36 @@ import mouse from '../asset/mouse.svg'
 import line from '../asset/line.svg'
 import rectangle from '../asset/rectangle.svg'
 
-interface State{
-
-}
-interface Prpos{
-    currentTool:string;
-    set:(arg0: string)=>void;
+interface Props{
+  currentTool:string;
+  set:(arg0: string)=>void;
 }
 
-export default class ToolbarContainer extends React.Component<Prpos,State> {
+const ToolbarContainer:  React.FC<Props> = (props) => {
 
-    handleClick(e:any){
-        this.props.set(e.target.id)
-    }
+  function handleClick(e:any){
+    props.set(e.target.id)
+  }
 
-    render() {
-
-        return (
-                <div className="toolbar">
-                    <div className="toolitem" onClick={this.handleClick.bind(this)} >
-                        <img className={this.props.currentTool==="mouse"?"itmeIconselect":"itmeIcon"} id="mouse"  alt="mouse" src={mouse}/>
-                    </div>
-                    <div className="toolitem" onClick={this.handleClick.bind(this)}>
-                        <img className={this.props.currentTool==="circle"?"itmeIconselect":"itmeIcon"} id="circle" alt="circle" src={circle}/>
-                    </div>
-                    <div className="toolitem" onClick={this.handleClick.bind(this)}>
-                        <img className={this.props.currentTool==="rectangle"?"itmeIconselect":"itmeIcon"} id="rectangle" alt="rectangle" src={rectangle}/>
-                    </div>
-                    <div className="toolitem" onClick={this.handleClick.bind(this)}>
-                        <img className={this.props.currentTool==="line"?"itmeIconselect":"itmeIcon"} id="line" alt="line" src={line}/>
-                    </div>
-                    <div className="toolitem" onClick={this.handleClick.bind(this)}>
-                        <img className={this.props.currentTool==="pen"?"itmeIconselect":"itmeIcon"} id="pen" alt="pen" src={pen}/>
-                    </div>
-                </div>
-        )
-    }
+  return (
+    <div className="toolbar">
+        <div className="toolitem" onClick={handleClick} >
+            <img className={props.currentTool==="mouse"?"itmeIconselect":"itmeIcon"} id="mouse"  alt="mouse" src={mouse}/>
+        </div>
+        <div className="toolitem" onClick={handleClick}>
+            <img className={props.currentTool==="circle"?"itmeIconselect":"itmeIcon"} id="circle" alt="circle" src={circle}/>
+        </div>
+        <div className="toolitem" onClick={handleClick}>
+            <img className={props.currentTool==="rectangle"?"itmeIconselect":"itmeIcon"} id="rectangle" alt="rectangle" src={rectangle}/>
+        </div>
+        <div className="toolitem" onClick={handleClick}>
+            <img className={props.currentTool==="line"?"itmeIconselect":"itmeIcon"} id="line" alt="line" src={line}/>
+        </div>
+        <div className="toolitem" onClick={handleClick}>
+            <img className={props.currentTool==="pen"?"itmeIconselect":"itmeIcon"} id="pen" alt="pen" src={pen}/>
+        </div>
+    </div>
+  )
 }
+
+export default ToolbarContainer;
