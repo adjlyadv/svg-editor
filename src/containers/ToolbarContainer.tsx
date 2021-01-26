f4a8ae615dac9c81661c0493d106692e7748feb3
Scentist25 <544708961@qq.com>
2021-01-26 14:15:12 +0800

    将class component改成function component

--- src/containers/ToolbarContainer.tsx
+++ src/containers/ToolbarContainer.tsx
@@ -6,40 +6,40 @@ import mouse from '../asset/mouse.svg'
 import line from '../asset/line.svg'
 import rectangle from '../asset/rectangle.svg'
 
-interface State{
 
-}
+
+
 interface Prpos{
     currentTool:string;
     set:(arg0: string)=>void;
 }
 
-export default class ToolbarContainer extends React.Component<Prpos,State> {
+export default function ToolbarContainer(props:Prpos){
 
-    handleClick(e:any){
-        this.props.set(e.target.id)
+    const handleClick=(e:any)=>{
+        props.set(e.target.id)
     }
 
-    render() {
+
 
         return (
                 <div className="toolbar">
-                    <div className="toolitem" onClick={this.handleClick.bind(this)} >
-                        <img className={this.props.currentTool==="mouse"?"itmeIconselect":"itmeIcon"} id="mouse"  alt="mouse" src={mouse}/>
+                    <div className="toolitem" onClick={handleClick} >
+                        <img className={props.currentTool==="mouse"?"itmeIconselect":"itmeIcon"} id="mouse"  alt="mouse" src={mouse}/>
                     </div>
-                    <div className="toolitem" onClick={this.handleClick.bind(this)}>
-                        <img className={this.props.currentTool==="circle"?"itmeIconselect":"itmeIcon"} id="circle" alt="circle" src={circle}/>
+                    <div className="toolitem" onClick={handleClick}>
+                        <img className={props.currentTool==="circle"?"itmeIconselect":"itmeIcon"} id="circle" alt="circle" src={circle}/>
                     </div>
-                    <div className="toolitem" onClick={this.handleClick.bind(this)}>
-                        <img className={this.props.currentTool==="rectangle"?"itmeIconselect":"itmeIcon"} id="rectangle" alt="rectangle" src={rectangle}/>
+                    <div className="toolitem" onClick={handleClick}>
+                        <img className={props.currentTool==="rectangle"?"itmeIconselect":"itmeIcon"} id="rectangle" alt="rectangle" src={rectangle}/>
                     </div>
-                    <div className="toolitem" onClick={this.handleClick.bind(this)}>
-                        <img className={this.props.currentTool==="line"?"itmeIconselect":"itmeIcon"} id="line" alt="line" src={line}/>
+                    <div className="toolitem" onClick={handleClick}>
+                        <img className={props.currentTool==="line"?"itmeIconselect":"itmeIcon"} id="line" alt="line" src={line}/>
                     </div>
-                    <div className="toolitem" onClick={this.handleClick.bind(this)}>
-                        <img className={this.props.currentTool==="pen"?"itmeIconselect":"itmeIcon"} id="pen" alt="pen" src={pen}/>
+                    <div className="toolitem" onClick={handleClick}>
+                        <img className={props.currentTool==="pen"?"itmeIconselect":"itmeIcon"} id="pen" alt="pen" src={pen}/>
                     </div>
                 </div>
         )
-    }
+
 }
