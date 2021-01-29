83a0b44c6cbeea8f3f765b402897636ad78fd6d0
kongwy <1419906205@qq.com>
2021-01-29 17:33:43 +0800

    Kwy/dragging mode (#15)
    * 增加拖拽锚点事件
    
    * add node dragging

--- src/containers/EditorContainer.tsx
+++ src/containers/EditorContainer.tsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useRef } from 'react';
-import { UIStore } from '../stores/UIStore';
+import React, { useEffect, useRef , useState} from 'react';
+import { Node as typeNode,UIStore } from '../stores/UIStore';
 import Path from '../elements/path';
 import '../style/EditorContainer.scss';
+import { getRelativePositon } from '../utils/calculate';
 
 const EditorContainer: React.FC<{}> = () =>  {
-
   useEffect(() => {
     if (!edtiorRef) {
       return
     }
-
     const editorInfo = edtiorRef?.current?.getBoundingClientRect();
-
     if (editorInfo) {
       UIStore.editorInfo.top = editorInfo.top;
       UIStore.editorInfo.left = editorInfo.left;
@@ -19,21 +17,71 @@ const EditorContainer: React.FC<{}> = () =>  {
   }, [])
 
   const edtiorRef = useRef<SVGSVGElement>(null);
-
-  
   const editorInfo = UIStore.editorInfo;
   const pathList = UIStore.pathList;
+  let pathid = UIStore.mouseState.pathid;
+  let nodeid = UIStore.mouseState.nodeid;
 
+  const [node, setNode] = useState<typeNode>(pathList[UIStore.mouseState.pathid].nodes[UIStore.mouseState.nodeid]);
+  
+  
+  useEffect(() => {
+    UIStore.setNodes(UIStore.mouseState.pathid, UIStore.mouseState.nodeid, node);
+  }, [node])
+
+  const handleMouseDown = (event: any) => {
+    event.stopPropagation();
+    pathid = UIStore.mouseState.pathid;
+    nodeid = UIStore.mouseState.nodeid;
+    let node1 = UIStore.pathList[pathid].nodes[nodeid];
+    setNode({
+      posX: node1.posX,
+      posY: node1.posY,
+      ctrPosX: node1.ctrPosX,
+      ctrPosY: node1.ctrPosY
+    });
+  }
+
+  const handleMouseMove = (event: any) => {
+    event.stopPropagation();
+    if(!UIStore.mouseState.drugging){
+      return
+    }
+    const { x, y } = getRelativePositon(event);
+    //锚点
+    if(UIStore.mouseState.type){
+    setNode({
+      posX: x,
+      posY: y,
+      ctrPosX: node.ctrPosX,
+      ctrPosY: node.ctrPosY
+    });
+  }
+  //控制点
+  else{
+    setNode({
+      posX: node.posX,
+      posY: node.posY,
+      ctrPosX: x,
+      ctrPosY: y
+    })
+  }
+
+}
+  
+  const handleMouseUp = (event: any) => {
+    event.stopPropagation();
+    UIStore.setMouseState(false,false,pathid,nodeid);
+  }
   return(
     <div className="editor-container">
-      <svg ref={edtiorRef} className="editor-svg" width={editorInfo.width} height={editorInfo.height}> 
+      <svg ref={edtiorRef} className="editor-svg" width={editorInfo.width} height={editorInfo.height} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}> 
         {pathList.map(path => (
           <Path path={path} />
         ))}
       </svg>
     </div>
   )
-
 }
 
 export default EditorContainer;
--- src/elements/node.tsx
+++ src/elements/node.tsx
@@ -1,7 +1,6 @@
-import { observer } from 'mobx-react';
-import React, { Fragment, useState } from 'react';
+import { observer} from 'mobx-react';
+import React, { Fragment } from 'react';
 import { Node as typeNode, UIStore } from '../stores/UIStore';
-import { getRelativePositon } from '../utils/calculate';
 
 interface Props {
   node: typeNode,
@@ -9,47 +8,25 @@ interface Props {
   pathId: number
 }
 
-
 const Node = observer((props: Props) => {
 
-  const [draging, setDraging ] = useState<boolean>(false);
-  const [node, setNode] = useState<typeNode>(props.node);
+  const node = UIStore.pathList[props.pathId].nodes[props.id]; 
 
-
-  const handleMouseDown = (event: any) => {
-    event.stopPropagation();
-    setDraging(true)
-    
-  }
-
-  const handleMouseMove = (event: any) => {
-    event.stopPropagation();
-    if (!draging) {
-      return
+  const handleMouseDown = (elec:boolean, event: any) => {
+    if(elec){
+      UIStore.setMouseState(true,true,props.pathId,props.id);
+      console.log("按下去锚点属于路径"+props.pathId+"的锚点"+props.id);
+    }else{
+      UIStore.setMouseState(false,true,props.pathId,props.id);
+      console.log("按下去控制点属于路径"+props.pathId+"的锚点"+props.id)
     }
-
-    const { x, y } = getRelativePositon(event);
-    setNode({
-      posX: x,
-      posY: y,
-      ctrPosX: node.ctrPosX,
-      ctrPosY: node.ctrPosY
-    })
-
-    UIStore.setNodes(0, props.id, node) // 需要进行节流
   }
-
-  const handleMouseUp = (event: any) => {
-    event.stopPropagation();
-    setDraging(false);
-  }
-
-
+  
   return(
     <Fragment>
-      <circle onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp} cx={node.posX} cy={node.posY} stroke="#55f" r="4" />
+      <circle onMouseDown={(e) => handleMouseDown(true, e)} cx={node.posX} cy={node.posY} stroke="#55f" r="4" />
         <line x1={node.posX} y1={node.posY} x2={node.ctrPosX} y2={node.ctrPosY} stroke="#555" strokeWidth="1" />
-      <circle cx={node.ctrPosX} cy={node.ctrPosY} stroke="#55f" r="4" />
+      <circle onMouseDown={(e) => handleMouseDown(false, e)} cx={node.ctrPosX} cy={node.ctrPosY} stroke="#55f" r="4" />
     </Fragment>
   )
 
