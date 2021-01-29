3afcaf02ed85fe16cc71cf9b8fc614639f40c4a5
kongwy229 <1419906205@qq.com>
2021-01-29 14:58:46 +0800

    增加拖拽锚点事件

--- src/stores/UIStore.ts
+++ src/stores/UIStore.ts
@@ -22,13 +22,20 @@ class UIstore {
     left: 0,
     top: 0
   }
+  mouseState ={
+    type:false,
+    drugging:false,
+    pathid: 0,
+    nodeid: 0
+  }
+
   pathList: Path[] = [];
 
   constructor() {
     makeAutoObservable(this);
     this.pathList.push(
       {
-        id: 1,
+        id: 0,
         nodes: [
           {
             posX: 157.5,
@@ -41,23 +48,51 @@ class UIstore {
             posY: 74,
             ctrPosX: 148.8,
             ctrPosY: 76.5,
-          },
+          }
         ],
         strokeWidth: 5,
         stroke: "#000000",
-        fill: "#ffffff"
+        fill: "none"
+      }
+    )
+
+    this.pathList.push(
+      {
+        id: 1,
+        nodes: [
+          {
+            posX: 257.5,
+            posY: 205.5,
+            ctrPosX: 280,
+            ctrPosY: 195,
+          },
+          {
+            posX: 327,
+            posY: 174,
+            ctrPosX: 248.8,
+            ctrPosY: 176.5,
+          }
+        ],
+        strokeWidth: 5,
+        stroke: "#000000",
+        fill: "none"
       }
     )
 
   }
-  
-
   setEditorInfo = (width: number, height: number) => {
     
   }
 
   setNodes = (pathId: number, nodeId: number, node: Node) => {
     this.pathList[pathId].nodes[nodeId] = node;
+  }
+
+  setMouseState = (type:boolean,dragging:boolean,pathid:number,nodeid:number) => {
+    this.mouseState.pathid = pathid;
+      this.mouseState.nodeid = nodeid;
+      this.mouseState.type = type;//为真是锚点，为假是控制点
+      this.mouseState.drugging = dragging;
   }
   setStateInfo = (pathId: number, name:string, value:string) => {
     switch(name){
