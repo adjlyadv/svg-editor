bcc78228d944ae00d93d8ab0e9f06404c7a425de
kongwy229 <1419906205@qq.com>
2021-03-07 21:11:02 +0800

    增加旋转路径功能

--- src/utils/calculate.ts
+++ src/utils/calculate.ts
@@ -15,6 +15,54 @@ export function getCentralSymmetryPosition(node: Node) {
   }
 }
 
+export function getAngle(node:Node){
+  const dot = node.posX * node.ctrPosX + node.posY * node.ctrPosY
+  const det = node.posX * node.ctrPosY - node.posY * node.ctrPosY
+  const angle = Math.atan2(det, dot) / Math.PI * 180
+  return (angle + 360) % 360
+}
+
+export function calRotatePath(pathid:number){
+  const path = UIStore.pathList[pathid];
+  const angle = path.rotate;
+  const center = path.centerPoint;
+  const border = path.border;
+
+  const  l = (angle * Math.PI) / 180;
+  const  cosv =  Math.cos(l);
+  const  sinv =  Math.sin(l);
+
+  let newNode = {
+    posX:-1,
+    posY:-1,
+    ctrPosX: -1,
+    ctrPosY: -1,
+    ctr2PosX: -1,
+    ctr2PosY: -1
+  }
+  path.nodes.forEach((item,index)=>{
+    newNode.posX = (item.posX - center.ctrx) * cosv - (item.posY - center.ctry) * sinv + center.ctrx;
+    newNode.posY =  (item.posX - center.ctrx) * sinv + (item.posY - center.ctry) * cosv + center.ctry;      
+    newNode.ctrPosX = (item.ctrPosX - center.ctrx) * cosv - (item.ctrPosY - center.ctry) * sinv + center.ctrx;
+    newNode.ctrPosY = (item.ctrPosX - center.ctrx) * sinv + (item.ctrPosY - center.ctry) * cosv + center.ctry;
+    if(item.ctr2PosX && item.ctr2PosY){
+      newNode.ctr2PosX = (item.ctr2PosX - center.ctrx) * cosv - (item.ctr2PosY - center.ctry) * sinv + center.ctrx;
+      newNode.ctr2PosY =  (item.ctr2PosX - center.ctrx) * sinv + (item.ctr2PosY - center.ctry) * cosv + center.ctry;
+      UIStore.setNodes(pathid,index,newNode);
+    }else{
+      UIStore.setNodes(pathid,index,{posX:newNode.posX, posY:newNode.posY, ctrPosX: newNode.ctrPosX, ctrPosY:newNode.ctrPosY });
+    }
+  })
+
+  let newBorder = [{ctrx:-1, ctry:-1},{ctrx:-1, ctry:-1},{ctrx:-1, ctry:-1},{ctrx:-1, ctry:-1}];
+  border.forEach((item,index)=>{
+    newBorder[index].ctrx = (item.ctrx - center.ctrx) * cosv - (item.ctry - center.ctry) * sinv + center.ctrx;
+    newBorder[index].ctry = (item.ctrx - center.ctrx) * sinv + (item.ctry - center.ctry) * cosv + center.ctry;
+  })
+  UIStore.setBorder(pathid,newBorder);
+}
+
+
 export function getCircleNodes(node: Node){
   const C = 0.552284749831;  
   let {posX,posY,ctrPosY,ctrPosX} = node;
